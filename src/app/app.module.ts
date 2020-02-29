e725c25ba0b77a985164b00d883fdf09ce75926c
Dmitry Romanov <me@wtrn.ru>
2020-02-29 18:13:04 +0300

    Add co2-widget.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { TimeWidgetComponent } from './widgets/time-widget/time-widget.component
 import { BudgetWidgetComponent } from './widgets/budget-widget/budget-widget.component';
 import { OfflineWidgetComponent } from './widgets/offline-widget/offline-widget.component';
 import {AbstractTextWidgetComponent} from "./widgets/abstract/text-widget/abstract-text-widget.component";
+import { Co2WidgetComponent } from './widgets/co2-widget/co2-widget.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import {AbstractTextWidgetComponent} from "./widgets/abstract/text-widget/abstra
     TimeWidgetComponent,
     BudgetWidgetComponent,
     OfflineWidgetComponent,
-    AbstractTextWidgetComponent
+    AbstractTextWidgetComponent,
+    Co2WidgetComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/widgets/co2-widget/co2-widget.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-co2-widget',
+  templateUrl: './co2-widget.component.html',
+  styleUrls: ['./co2-widget.component.styl']
+})
+export class Co2WidgetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
